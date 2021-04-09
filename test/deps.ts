7ab193ecf3cbaf825deaf48d46966e97019c4839
Simon Skinner <vultuk28@gmail.com>
2021-04-09 13:47:29 +0100

    File names

--- test/deps.ts
+++ test/deps.ts
@@ -1,12 +1,12 @@
-// export {
-//   Microservice,
-//   Cors,
-// } from 'https://deno.land/x/microservice/http/Mod.ts';
-// export type {
-//   Request,
-//   Response,
-//   Route,
-// } from 'https://deno.land/x/microservice/http/Mod.ts';
+export {
+  Microservice,
+  Cors,
+} from 'https://deno.land/x/microservice/http/Mod.ts';
+export type {
+  Request,
+  Response,
+  Route,
+} from 'https://deno.land/x/microservice/http/Types.ts';
 
-export { Microservice, Cors } from '../http/Mod.ts';
-export type { Request, Response, Route } from '../http/Types.ts';
+// export { Microservice, Cors } from '../http/Mod.ts';
+// export type { Request, Response, Route } from '../http/Types.ts';
